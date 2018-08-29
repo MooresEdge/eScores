ca1808547c2f3cf4ee158446a589a15186193ca9
Nathan Moore - Laptop <mooresedge@gmail.com>
2018-08-29 16:08:51 +0100

    working buttons

--- js.js
+++ js.js
@@ -4,6 +4,9 @@
 var group = document.querySelectorAll(".btn-group-toggle label");
 
 group.forEach(function(entry) {
-    entry.addEventListener("click", function(){ entry.classList.add("active")});
+    //entry.addEventListener("click", function(){ entry.classList.add("active")});
     //entry.classList.add("active");
+    //$(".btn-group-toggle label").button('toggle');
 });
+
+//$(".btn-group-toggle label").button('toggle');
