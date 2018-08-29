18421f4dc677f4c97d462368209dfe490c3c9d1e
Nathan Moore - Laptop <mooresedge@gmail.com>
2018-08-29 15:44:19 +0100

    button event

--- js.js
+++ js.js
@@ -1,1 +1,9 @@
 //alert( "I'm JavaScript!" );
+//document.querySelectorAll(".btn-group-toggle label").classList.add("active");
+
+var group = document.querySelectorAll(".btn-group-toggle label");
+
+group.forEach(function(entry) {
+    entry.addEventListener("click", function(){ entry.classList.add("active")});
+    //entry.classList.add("active");
+});
