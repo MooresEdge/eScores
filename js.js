990a686bb90502762d2f824a54e03ffd75617253
Nathan Moore - Laptop <mooresedge@gmail.com>
2018-08-30 15:12:31 +0100

    odds calc added

--- js.js
+++ js.js
@@ -10,3 +10,30 @@ group.forEach(function(entry) {
 });
 
 //$(".btn-group-toggle label").button('toggle');
+
+function updateOdds(obj){
+
+    //console.log(obj);
+    var odds = 1.00;
+    //console.log(obj.classList.contains("active"));
+
+    var group = document.querySelectorAll(".btn-group-toggle label");
+    for(var i = 0; i < group.length; i++)
+    {
+       if(group[i].children[0].checked)
+       {
+           var odd = group[i].children[1].textContent;
+           oddValue = 1 + eval(odd);
+           odds *= oddValue;
+       }
+    }
+    //odds -= 1;
+    //console.log(document.querySelector("#odds").children[0].textContent);
+    document.querySelector("#odds").children[0].textContent = odds.toFixed(2) + "";
+}
+
+function calculateReturn(obj)
+{
+    var total = obj.value * document.querySelector("#odds").children[0].textContent;
+    document.querySelector("#returns").textContent = "£" + total.toFixed(2);
+}
